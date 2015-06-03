210164ee5e308db243d7d4d62196cabd49385e5a
Rinesh Ramadhin <Rinesh.Ramadhin@gmail.com>
2015-06-03 13:04:17 +0200

    Creating project
    Smart radio controller console

---
+++ Smart radio controller console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_radio_controller_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
